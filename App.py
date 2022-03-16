0b7cfd697e9c8990a2687081c2afa3f7b0c64440
angela-r <angelaivonneromerogarzon@gmail.com>
2022-03-16 14:43:06 -0500

    versión 1

---
+++ App.py
@@ -0,0 +1,88 @@
+from asyncio.windows_events import NULL
+from calendar import weekheader
+from flask import Flask, request, json
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+
+#MySQL Connection
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'a2censo'
+mysql= MySQL(app)
+
+# Settings
+
+app.secret_key = 'mysecretkey'
+
+
+@app.route ('/list_campanign', methods=['GET'])
+def list_campanign():
+    print('inicio lista')
+    try:
+        amount = request.form['amount']
+    except:
+        amount = None
+    try:
+        requested_Amount = request.form['requested_Amount']
+    except:
+        requested_Amount = None
+    try:
+        order = request.form['order']
+    except:
+        ordert = None
+    cur = mysql.connection.cursor()
+    query = 'SELECT * FROM campaign'
+    select = ''
+    where = ''
+    if amount is not None:
+        select +=  'amount'
+        where += 'amount = '+str(amount)
+    if requested_Amount is not None:
+        select +=  ',requested_Amount'
+        where += ' and requested_Amount = '+str(requested_Amount)
+    #if where != '':
+        #query += ' where ' + where
+    if order is not None:
+        if amount is not None or requested_Amount is not None:
+            query += ' order by '+select+' '+ order
+    print(query)
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    #return render_template('index.html', contacts = data)
+    #flash('Campaña agregada satisfactoriamente')
+    response = app.response_class(
+            response=json.dumps(data),
+            status=200,
+            mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/add_campanign', methods=['POST'])
+def add_campanign():
+    print('iniciar')
+    if request.method =='POST':
+        name = request.form['name']
+        amount = request.form['amount']
+        requested_Amount = request.form ['requested_Amount']
+        admin_Rate = request.form ['admin_Rate']
+        cur = mysql.connection.cursor()
+        query = "INSERT INTO campaign (name, amount, requested_Amount) VALUES ('%s', %s, %s)" % ( name, amount, requested_Amount )
+        print(query)
+        cur.execute(query)
+        mysql.connection.commit()
+        #flash('Campaña agregada satisfactoriamente')
+        response = app.response_class(
+            response=json.dumps({'msn':'creado'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# Prueba_AR_BVC
+#  Pasos a seguir para compilar 
+
+1. Install pip virtualenv
+``` 
+pip install virtualenv
+``` 
+2. creat virtual env
+``` 
+virtualenv env
+``` 
+3. seleccionar env
+``` 
+# Linux
+source env/bin/activate
+
+# windows
+.\env\Scripts\activate  
+
+``` 
+4. instalar paquetes relacionados
+``` 
+pip install -r requirements.txt
+``` 
+
+
+# docker
+python -m http.server 8000
+docker build -t webserver .
+docker run -it --rm -d -p 8080:8080 --name web webserver
+
+docker build -t app-flask .
+
+
+
+docker run -it -p 5000:5000 app-flask
+
+docker run -it --rm -d -p 5050:5050  --name app-flask
+
+
+docker-compose build
+docker-compose up -d
+
+docker-compose up --force-recreate
+
+
+docker build . -t our-server
+docker run -it --rm -p 8080:80 cerbero
+
+
+# Documentacion gapi
+    https://developers.google.com/identity/sign-in/web/sign-in
+
